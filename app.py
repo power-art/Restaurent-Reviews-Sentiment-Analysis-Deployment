34c7c6a3c01172bd6fe58c7c11a03a1106018761
power-art <preetamdwivedi2@gmail.com>
2020-07-16 23:31:33 +0530

    create html page for making home page

--- app.py
+++ app.py
@@ -5,13 +5,14 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
-classfier = pickle.load(open("sentiment_analysis","rb"))
+classfier = pickle.load(open("sentiment_analysis.pkl","rb"))
 cv = pickle.load(open("cv-transform.pkl","rb"))
 
 app = Flask(__name__)
 
 @app.route('/')
-
+def home():
+	return render_template('home.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
